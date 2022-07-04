9382713f4865d9a9da646f27b77d7bbdc4b56bea
Raul Rohjans <rauljann@hotmail.com>
2022-07-04 01:02:36 +0100

    Fixed some small mistakes

--- run.sh
+++ run.sh
@@ -22,30 +22,35 @@ yes | sudo pacman -S firefox audacity bitwarden discord filezilla gnome-tweaks g
 
 yes | sudo pamac install --no-confirm anydesk-bin github-desktop-bin gnome-sound-recorder-git google-chrome gwe minecraft-launcher mpv-amd-full-git openvpn3 pavucontrol-git postman-bin spotify sublime-text-4 teams-for-linux zoom zpaq
 
+modprobe vboxdrv
+
 # Install app images
 # --Houdoku
+mkdir ~/.local/share/Houdoku
+cd ~/.local/share/Houdoku
+wget https://d33wubrfki0l68.cloudfront.net/73dfe4f08550667ac4d01ed21831662b33b0170b/c3de8/img/logo.svg
 cd /tmp/rohjans
 mkdir houdoku
+sudo chmod 777 -R houdoku
 cd houdoku
 curl -s https://api.github.com/repos/xgi/houdoku/releases/latest | grep "browser_download_url.*AppImage" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 for FILENAME in *; do mv $FILENAME "houdoku.AppImage"; done 
 mkdir ~/Applications
 mv *.AppImage ~/Applications
-mkdir ~/.local/share/applications
-cd ~/.local/share/applications
-printf "[Desktop Entry]\n
-Name=Houdoku\n
-Exec=/home/raul/Applications/houdoku.AppImage --no-sandbox %U\n
-Terminal=false\n
-Type=Application\n
-Icon=/home/raul/.local/share/Houdoku/logo.svg\n
-StartupWMClass=Houdoku\n
-X-AppImage-Version=2.9.0\n
-Comment=Manga reader and library manager for the desktop\n
-Categories=Development;\n\n
+cd /usr/share/applications
+sudo printf "[Desktop Entry]
+Name=Houdoku
+Exec=/home/raul/Applications/houdoku.AppImage --no-sandbox %%U
+Terminal=false
+Type=Application
+Icon=/home/raul/.local/share/Houdoku/logo.svg
+StartupWMClass=Houdoku
+X-AppImage-Version=2.9.0
+Comment=Manga reader and library manager for the desktop
+Categories=Development;
 
-TryExec=/home/raul/Applications/houdoku.AppImage\n
-X-AppImage-Old-Icon=houdoku\n
+TryExec=/home/raul/Applications/houdoku.AppImage
+X-AppImage-Old-Icon=houdoku
 X-AppImage-Old-Name=Houdoku" > houdoku.desktop
 
 # Install GPU drivers
@@ -74,7 +79,8 @@ mkdir ~/.config
 cp $config_path"/wallpapers/background" ~/.config
 
 # Import Gnome theme settings
-echo ${$(cat $config_path"/dconf-settings.ini")//raul/$shell_user} | dconf load /
+sed -i 's/raul/'$shell_user'/g' $config_path"/dconf-settings.ini"
+cat $config_path"/dconf-settings.ini" | dconf load /
 
 # Clean files & reboot
 sudo rm -rf /tmp/rohjans
